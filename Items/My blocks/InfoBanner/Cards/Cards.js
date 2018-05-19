b50e6b328678a9355a768c2c00a3ad495997c9c8
이주향 <grapevine0lee@gmail.com>
2018-05-19 12:20:22 +0900

    (am 1-3) 퀘스트 완료했습니다.

--- Items/My blocks/InfoBanner/Cards/Cards.js
+++ Items/My blocks/InfoBanner/Cards/Cards.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './Cards.scss'
+import './Cards.scss';
 
 const Cards = ({ children, className, ...other }) => {
   return (
     <div className="row grid-default-style">
-      <div className="col-6">card 1<br/>text</div>
-      <div className="col-6">card 2<br/>text</div>
+      <div className="col-6 info-banner-cards">card1<br/>text</div>
+      <div className="col-6 info-banner-cardsa">card2<br/>text</div>
     </div>
   )
   ;

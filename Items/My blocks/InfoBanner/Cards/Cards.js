5c04c40801690de4e2cbd0fee0175c364301a273
이주향 <grapevine0lee@gmail.com>
2018-05-19 09:39:44 +0900

    (am 1-1) 퀘스트 완료했습니다. :)

--- Items/My blocks/InfoBanner/Cards/Cards.js
+++ Items/My blocks/InfoBanner/Cards/Cards.js
@@ -3,8 +3,9 @@ import './Cards.scss'
 
 const Cards = ({ children, className, ...other }) => {
   return (
-    <div className='info-banner-cards'>
-      <h1>(Cards)</h1>
+    <div className="row grid-default-style">
+      <div className="col-6">column 01</div>
+      <div className="col-6">column 02</div>
     </div>
   )
   ;

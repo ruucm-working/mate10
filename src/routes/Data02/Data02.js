5eeae3b80a829fb93591f378a136cbeaaf81fed2
이주향 <grapevine0lee@gmail.com>
2018-05-19 16:35:59 +0900

    (am 2-7) 차트 크기와 위치 바꾸기 완료했습니다.

--- src/routes/Data02/Data02.js
+++ src/routes/Data02/Data02.js
@@ -11,11 +11,11 @@ const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
                   {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const RADIAN = Math.PI / 180;                    
+const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
  	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x  = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
  
   return (
     <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
@@ -42,15 +42,57 @@ class Data02Component extends Component {
           <InfoBanner.Cards />
         </InfoBanner>
         <h1>Data 02</h1>
-        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+        <PieChart width={350} height={80} onMouseEnter={this.onPieEnter}>
         <Pie
           data={data} 
-          cx={300} 
-          cy={200} 
+          cx="25"
+          cy="center"
           labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={80} 
-          innerRadius={50}
+          // label={renderCustomizedLabel}
+          outerRadius={30}
+          innerRadius={20}
+          fill="#8884d8"
+        >
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+        <Pie
+          data={data} 
+          cx="120"
+          cy="center"
+          labelLine={false}
+          // label={renderCustomizedLabel}
+          outerRadius={30}
+          innerRadius={20}
+          fill="#8884d8"
+        >
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+        <Pie
+          data={data} 
+          cx="215"
+          cy="center"
+          labelLine={false}
+          // label={renderCustomizedLabel}
+          outerRadius={30}
+          innerRadius={20}
+          fill="#8884d8"
+        >
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+        <Pie
+          data={data} 
+          cx="308"
+          cy="center"
+          labelLine={false}
+          // label={renderCustomizedLabel}
+          outerRadius={30}
+          innerRadius={20}
           fill="#8884d8"
         >
         	{

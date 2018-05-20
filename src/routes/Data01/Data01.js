38dd93b56c8b814f57bc7468e1076bb0f02f18bf
이주향 <grapevine0lee@gmail.com>
2018-05-20 15:21:49 +0900

    (am 3-6) Line chart 색, 랩 바꾸기 수정해서 다시 커밋합니당

--- src/routes/Data01/Data01.js
+++ src/routes/Data01/Data01.js
@@ -16,15 +16,6 @@ var obj = {
   },
 };
 
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
 export default class Data01Component extends Component {
   constructor() {
     super()
@@ -48,46 +39,22 @@ export default class Data01Component extends Component {
               draws: responseJson.standing[0].draws,
               losses: responseJson.standing[0].losses,
             },
-            {
-              name: 'BVB',
-              goalsAgainst: responseJson.standing[1].goalsAgainst,
-              goals: responseJson.standing[1].goals,
-              wins: responseJson.standing[1].wins,
-              draws: responseJson.standing[1].draws,
-              losses: responseJson.standing[1].losses,
-            },
-            {
-              name: 'Leverkusen',
-              goalsAgainst: responseJson.standing[2].goalsAgainst,
-              goals: responseJson.standing[2].goals,
-              wins: responseJson.standing[2].wins,
-              draws: responseJson.standing[2].draws,
-              losses: responseJson.standing[2].losses,
-            },
-            {
-              name: 'Gladbach',
-              goalsAgainst: responseJson.standing[3].goalsAgainst,
-              goals: responseJson.standing[3].goals,
-              wins: responseJson.standing[3].wins,
-              draws: responseJson.standing[3].draws,
-              losses: responseJson.standing[3].losses,
-            },
           ],
         });
       });
   }
   render() {
     return (
-      <div className="data-01">
+      <div className="use-data-default-style">
         <h1>2015-2016<br/>
         바이에른뮌헨의 최근 리그 승패 횟수</h1>
         {log('this.state 💎', this.state)}
-        <AreaChart width={335} height={200} data={this.state.data}>
-          <CartesianGrid strokeDasharray="3 3"/>
-          <XAxis dataKey="name"/>
-          <Tooltip/>
-          <Area type="monotone" dataKey="wins" stroke="#89a6ff"/>
-        </AreaChart>
+        <div className="recharts-wrapping">
+          <AreaChart width={335} height={200} data={this.state.data}>
+            <Tooltip/>
+            <Area type="monotone" dataKey="wins" stroke="#89a6ff"/>
+          </AreaChart>
+        </div>
       </div>
     )
   }
